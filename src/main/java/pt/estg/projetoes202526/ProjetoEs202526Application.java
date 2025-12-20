49359ecc77a7e5ae59a6969ee7d7cafe93d91b92
Nuno Matos <86886446+nuno-matos@users.noreply.github.com>
2025-12-20 01:46:42 +0000

    Initial commit

---
+++ src/main/java/pt/estg/projetoes202526/ProjetoEs202526Application.java
@@ -0,0 +1,13 @@
+package pt.estg.projetoes202526;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEs202526Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEs202526Application.class, args);
+	}
+
+}
---
+++ src/test/java/pt/estg/projetoes202526/ProjetoEs202526ApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.estg.projetoes202526;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEs202526ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
