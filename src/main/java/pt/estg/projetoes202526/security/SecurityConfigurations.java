acd0da20c4a61bc14fc399e4f3f12529b6f02124
Nuno Matos <86886446+nuno-matos@users.noreply.github.com>
2026-01-06 01:55:40 +0000

    feat: add SecurityConfigurations, SecurityFilterChain

---
+++ src/main/java/pt/estg/projetoes202526/security/SecurityConfigurations.java
@@ -0,0 +1,26 @@
+package pt.estg.projetoes202526.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll() //Esta linha so serve para testes eliminar
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
