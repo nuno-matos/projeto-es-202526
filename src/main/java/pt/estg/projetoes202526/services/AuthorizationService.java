b68fa02e8fbc570f40938bb7715ce0cb02a9b8da
Nuno Matos <86886446+nuno-matos@users.noreply.github.com>
2026-01-06 01:42:34 +0000

    feat: add AuthorizationService that implements UserDetailsService with a method loadUserByUsername

---
+++ src/main/java/pt/estg/projetoes202526/services/AuthorizationService.java
@@ -0,0 +1,18 @@
+package pt.estg.projetoes202526.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pt.estg.projetoes202526.repositories.UserRepository;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username);
+    }
+}
