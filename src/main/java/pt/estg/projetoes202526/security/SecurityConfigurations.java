ccb7e247dd77bffc59b1bbe338f6bb8b58b6a3c9
Nuno Matos <86886446+nuno-matos@users.noreply.github.com>
2026-01-06 03:18:53 +0000

    feat: add BCryptPasswordEncoder to SecurityConfigurations

--- src/main/java/pt/estg/projetoes202526/security/SecurityConfigurations.java
+++ src/main/java/pt/estg/projetoes202526/security/SecurityConfigurations.java
@@ -6,11 +6,18 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
