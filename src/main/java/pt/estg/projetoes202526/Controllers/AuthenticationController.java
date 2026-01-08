a4da4532c614f8b6d55ab923d299707e422efba7
Nuno Matos <86886446+nuno-matos@users.noreply.github.com>
2026-01-08 01:07:50 +0000

    feat: add Post mapping logic for login page

--- src/main/java/pt/estg/projetoes202526/Controllers/AuthenticationController.java
+++ src/main/java/pt/estg/projetoes202526/Controllers/AuthenticationController.java
@@ -2,20 +2,30 @@ package pt.estg.projetoes202526.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pt.estg.projetoes202526.domain.dto.LoginDTO;
+import pt.estg.projetoes202526.domain.dto.LoginResponseDTO;
 import pt.estg.projetoes202526.domain.dto.RegisterDTO;
 import pt.estg.projetoes202526.domain.models.User;
 import pt.estg.projetoes202526.repositories.UserRepository;
+import pt.estg.projetoes202526.security.TokenService;
 
 @RestController
 @RequestMapping("auth")
 public class AuthenticationController {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private TokenService tokenService;
+
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody RegisterDTO request) {
@@ -24,6 +34,14 @@ public class AuthenticationController {
         User newUser = new User(request.email(), encryptedPassword, request.username(), request.role());
         userRepository.save(newUser);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody LoginDTO request){
+        var UsernamePassword = new UsernamePasswordAuthenticationToken(request.email(), request.password());
+        var authentication = authenticationManager.authenticate(UsernamePassword);
+        var token = tokenService.generateToken((User) authentication.getPrincipal());
+        return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
 }
