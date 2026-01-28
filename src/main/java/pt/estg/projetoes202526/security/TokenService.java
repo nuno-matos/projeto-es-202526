0e832d75a81ae35a4f482d2a5d5ff302229efaad
Nuno Matos <86886446+nuno-matos@users.noreply.github.com>
2026-01-28 01:45:15 +0000

    feat: add role to the token when it's generated

--- src/main/java/pt/estg/projetoes202526/security/TokenService.java
+++ src/main/java/pt/estg/projetoes202526/security/TokenService.java
@@ -4,6 +4,9 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import pt.estg.projetoes202526.domain.models.User;
@@ -14,17 +17,22 @@ import java.time.ZoneId;
 
 @Service
 public class TokenService {
+
+    private static final Logger logger = LoggerFactory.getLogger(TokenService.class);
+
     @Value("${api.security.token.secret}")
     private String secret;
     @Value("{spring.application.name}")
     private String appName;
 
     public String generateToken(User user) {
+        logger.info("User Role: {}", user.getRole().getName());
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             String token = JWT.create()
                     .withIssuer(appName)
                     .withSubject(user.getEmail())
+                    .withClaim("role", "ROLE_" + user.getRole().getName())
                     .withExpiresAt(generateExpirationDate())
                     .sign(algorithm);
             return token;
@@ -45,7 +53,6 @@ public class TokenService {
             return "";
         }
     }
-
     //Data de expiração do token e definido no plushours = 2 horas
     private Instant generateExpirationDate() {
         return LocalDateTime.now()
