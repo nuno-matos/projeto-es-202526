d8b7c611e8a097a048ecafcd53ee04009de2bf92
Nuno Matos <86886446+nuno-matos@users.noreply.github.com>
2026-02-03 23:58:51 +0000

    feat: add constructors, getters and setters

--- src/main/java/pt/estg/projetoes202526/domain/models/Exercise.java
+++ src/main/java/pt/estg/projetoes202526/domain/models/Exercise.java
@@ -18,4 +18,50 @@ public class Exercise {
 
     @OneToMany(mappedBy = "exercise", cascade = CascadeType.ALL)
     private Set<Question> questions = new HashSet<>();
+
+    public Exercise() {}
+    public Exercise(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<CourseUnit> getCourseUnits() {
+        return courseUnits;
+    }
+
+    public void setCourseUnits(Set<CourseUnit> courseUnits) {
+        this.courseUnits = courseUnits;
+    }
+
+    public Set<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(Set<Question> questions) {
+        this.questions = questions;
+    }
 }
