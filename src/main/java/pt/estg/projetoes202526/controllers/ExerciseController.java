ee41e46ea61d4d08a67d95055815eca73e4a4c80
Nuno Matos <86886446+nuno-matos@users.noreply.github.com>
2026-02-04 00:01:25 +0000

    feat: add ExerciseController with a method to register an exercise on the DB

---
+++ src/main/java/pt/estg/projetoes202526/controllers/ExerciseController.java
@@ -0,0 +1,50 @@
+package pt.estg.projetoes202526.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pt.estg.projetoes202526.domain.dto.ExerciseRegisterDTO;
+import pt.estg.projetoes202526.domain.models.Exercise;
+import pt.estg.projetoes202526.domain.models.Question;
+import pt.estg.projetoes202526.repositories.ExerciseRepository;
+
+@RestController
+@RequestMapping("api/exercise")
+public class ExerciseController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExerciseController.class);
+
+    @Autowired
+    private ExerciseRepository exerciseRepository;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> createExercise(@RequestBody ExerciseRegisterDTO request) {
+        Exercise exercise = new Exercise();
+        exercise.setTitle(request.exerciseTitle());
+        String description = "description";
+        exercise.setDescription(description);
+
+        request.questions().forEach(qDto -> {
+            logger.info(
+                    "Creating Question → group='{}', text='{}'",
+                    qDto.group(),
+                    qDto.questionText()
+            );
+            Question question = new Question();
+            question.setQuestionText(qDto.questionText());
+            question.setQuestionGroup(qDto.group());
+            question.setExercise(exercise);
+
+            exercise.getQuestions().add(question);
+        });
+
+        exerciseRepository.save(exercise);
+
+        return ResponseEntity.ok().build();
+    }
+}
